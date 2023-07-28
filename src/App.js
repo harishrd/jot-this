adee39399852f9e79bc6eed72a6c8a968809453e
harishrd <harishrd321@gmail.com>
2023-07-29 00:13:24 +0530

    add save note feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 
 const App = () => { 
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "28/07/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "29/07/2023",
+    },
+    {
+      id: nanoid(),
+      // time: "This is my third note!",
+      text: "This is my third note!",
+      date: "30/07/2023",
+    }
+  ]);
+
+  // since the state lives in top level component, we need to pass the function down to the child component
+  // using prop drilling we can pass the function down to the child component
+  const addNote = (text) => { 
+    const date = new Date();
+    const newNote = {
+      text: text,
+      date: date.toLocaleDateString(),
+      id: nanoid()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={ notes } handleAddNote={ addNote } />
     </div>
   );
 
