7892aa98a0289ef18f426d3649503e211f6dfe9f
harishrd <harishrd321@gmail.com>
2023-08-01 12:33:25 +0530

    fixed edit button

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -18,10 +18,8 @@ const Note = ({ _id, text, date, handleDeleteNote, handleEditNote }) => {
         setIsEditing(false);
     };
 
-    
-
     return (
-        <div className={`note ${isEditing && "editing"}`}>
+        <div className={`note ${isEditing ? "editing" : ""}`}>
             {!isEditing ? (
                 <span>{text}</span>
             ) : (
@@ -34,23 +32,86 @@ const Note = ({ _id, text, date, handleDeleteNote, handleEditNote }) => {
             )}
             <div className="note-footer">
                 <small>{date}</small>
-                {!isEditing ? (
-                    <>
-                        <MdEdit onClick={onEditClick} className="edit-icon" size="1.3em" />
-                        <MdDeleteForever
-                            onClick={onDeleteClick}
-                            className="delete-icon"
-                            size="1.3em"
-                        />
-                    </>
-                ) : (
-                    <button className="save" onClick={onSaveClick}>
-                        Save
-                    </button>
-                )}
+                <div className="edit-options">
+                    {!isEditing ? (
+                        <>
+                            <MdEdit onClick={onEditClick} className="icon edit-icon" size="1.3em" />
+                            <MdDeleteForever
+                                onClick={onDeleteClick}
+                                className="icon delete-icon"
+                                size="1.3em"
+                            />
+                        </>
+                    ) : (
+                        <button className="icon save" onClick={onSaveClick}>
+                            Save
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
 };
 
 export default Note;
+
+
+
+// import React, { useState } from "react";
+// import { MdDeleteForever, MdEdit } from "react-icons/md";
+
+// const Note = ({ _id, text, date, handleDeleteNote, handleEditNote }) => {
+//     const [isEditing, setIsEditing] = useState(false);
+//     const [editedText, setEditedText] = useState(text);
+
+//     const onDeleteClick = () => {
+//         handleDeleteNote(_id);
+//     };
+
+//     const onEditClick = () => {
+//         setIsEditing(true);
+//     };
+
+//     const onSaveClick = () => {
+//         handleEditNote(editedText);
+//         setIsEditing(false);
+//     };
+
+    
+
+//     return (
+//         <div className={`note ${isEditing && "editing"}`}>
+//             {!isEditing ? (
+//                 <span>{text}</span>
+//             ) : (
+//                 <textarea
+//                     rows="4"
+//                     cols="10"
+//                     value={editedText}
+//                     onChange={(e) => setEditedText(e.target.value)}
+//                 />
+//             )}
+//             <div className="note-footer">
+//                 <small>{date}</small>
+//                 <div className="edit-options">
+//                     {!isEditing ? (
+//                         <>
+//                             <MdEdit onClick={onEditClick} className="icon edit-icon" size="1.3em" />
+//                             <MdDeleteForever
+//                                 onClick={onDeleteClick}
+//                                 className="delete-icon"
+//                                 size="1.3em"
+//                             />
+//                         </>
+//                     ) : (
+//                         <button className="icon save" onClick={onSaveClick}>
+//                             Save
+//                         </button>
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Note;
