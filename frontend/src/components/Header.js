cca1e08e5b02dcadea58d3e3bbd97c2517d05483
harishrd <harishrd321@gmail.com>
2023-08-02 10:18:46 +0530

    added build

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,27 +2,6 @@ import React from "react";
 import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
 
 
-// const Header = ({ handleToggleDarkMode }) => { 
-//     return (
-//         <div className="header">
-//             <h1>JotThis.</h1>
-//             <button
-//                 onClick={() =>
-//                     handleToggleDarkMode(
-//                         (previousDarkMode) => !previousDarkMode
-//                     )
-//                 }
-//                 className="toggle save"
-//             >
-//                 Toggle Mode
-//             </button>
-
-//         </div>
-//     );
-// };
-
-// export default Header;
-
 import { useState } from "react"; // Make sure to import 'useState'
 
 const Header = ({ handleToggleDarkMode }) => {
@@ -60,3 +39,26 @@ const Header = ({ handleToggleDarkMode }) => {
 };
 
 export default Header;
+
+
+
+// const Header = ({ handleToggleDarkMode }) => { 
+//     return (
+//         <div className="header">
+//             <h1>JotThis.</h1>
+//             <button
+//                 onClick={() =>
+//                     handleToggleDarkMode(
+//                         (previousDarkMode) => !previousDarkMode
+//                     )
+//                 }
+//                 className="toggle save"
+//             >
+//                 Toggle Mode
+//             </button>
+
+//         </div>
+//     );
+// };
+
+// export default Header;
